42ad6186e69a82559361d20aa783330e1986a627
Dilan Espinoza <dilan.developer22@gmail.com>
2025-04-14 15:49:28 -0500

    change password db

--- src/server.js
+++ src/server.js
@@ -5,11 +5,6 @@ import imageRoutes from "./routes/images.routes.js";
 import { errorHandler } from "./middlewares/errorHandler.js";
 import cors from "cors";
 import path from "path";
-import db from "./config/db.js";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
 
 const app = express();
 const __dirname = path.resolve();
