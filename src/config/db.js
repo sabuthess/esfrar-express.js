2a761efe5ef87579a0fc4890efd124751e87196b
Dilan Espinoza <dilan.developer22@gmail.com>
2025-04-13 23:36:48 -0500

    change paths

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,7 @@
 import mysql from "mysql2";
 import config_db from "./dotenv.js"; // Importa las credenciales
 
-const connection = await mysql.createConnection({
+const pool = mysql.createPool({
 	host: config_db.db.host,
 	user: config_db.db.user,
 	password: config_db.db.password,
@@ -9,12 +9,13 @@ const connection = await mysql.createConnection({
 	connectTimeout: 10000,
 });
 
-connection.connect((err) => {
+pool.getConnection((err, connection) => {
 	if (err) {
 		console.error("❌ Error al conectar a MySQL:", err);
-		process.exit(1); // Detiene la app si la conexión falla
+		process.exit(1);
 	}
-	console.log("✅ Conectado a MySQL");
+	console.log("✅ Conectado a MySQL (con pool)");
+	connection.release(); // libera la conexión al pool
 });
 
-export default connection;
+export default pool;
