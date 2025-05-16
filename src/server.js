571f03cea67cf29f529df76e02f57668a1df6ce9
Dilan Espinoza <dilan.developer22@gmail.com>
2025-05-15 19:19:20 -0500

    resolve conflict to cors

--- src/server.js
+++ src/server.js
@@ -9,13 +9,14 @@ import path from "path";
 const app = express();
 const __dirname = path.resolve();
 
-const corsOptions = {
-	origin: "https://esfrar-next-js.vercel.app",
+/* const corsOptions = {
+	origin: "http:localhost:3000",
 	methods: "GET,POST,PUT,DELETE",
 	allowedHeaders: "Content-Type,Authorization",
-};
+}; */
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
