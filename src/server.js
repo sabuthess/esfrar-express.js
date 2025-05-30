03c1efe451f6f8c6afa65a7bc3b2e6f471b451ac
Dilan Espinoza <dilan.developer22@gmail.com>
2025-05-29 22:52:30 -0500

    feat: add image likes

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import config_db from "./config/dotenv.js";
 import userRoutes from "./routes/user.routes.js";
 import imageRoutes from "./routes/images.routes.js";
+import imageLikesRoutes from "./routes/imageLikes.routes.js";
 import { errorHandler } from "./middlewares/errorHandler.js";
 import cors from "cors";
 import path from "path";
@@ -22,8 +23,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use("/api", userRoutes);
 app.use("/api", imageRoutes);
+app.use("/api", imageLikesRoutes);
 app.use(errorHandler); // Middleware de errores
 
 app.listen(config_db.port, () => {
-	console.log(`🚀 Servidor corriendo en http://localhost:${config_db.port}`);
+	console.log(
+		`🚀 Servidor corriendo en ${process.env.DB_HOST}:${config_db.port}`
+	);
 });
