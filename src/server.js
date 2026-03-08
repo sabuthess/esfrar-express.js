c2436a977c49c862e31a377d76833863599fb269
sabuthess <dilan.developer22@gmail.com>
2026-03-08 15:34:25 -0500

    Revome uses images likes and images favorites routes

--- src/server.js
+++ src/server.js
@@ -1,9 +1,7 @@
 import express from "express";
 import config_db from "./config/dotenv.js";
 import userRoutes from "./routes/user.routes.js";
-import imagesRoutes from "./routes/images.routes.js"; // imágenes
-import likesRoutes from "./routes/imagesLikes.routes.js"; // likes
-import favoritesRoutes from "./routes/imagesFavorites.routes.js"; // favoritos
+import imagesRoutes from "./routes/images.routes.js";
 import { errorHandler } from "./middlewares/errorHandler.js";
 import cors from "cors";
 import path from "path";
@@ -18,8 +16,7 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 app.use("/api/users", userRoutes);
 app.use("/api/images", imagesRoutes);
-app.use("/api/images", likesRoutes); // Rutas de likes para imágenes
-app.use("/api/images", favoritesRoutes); // Rutas de favoritos para imágenes
+
 
 app.use(errorHandler); // Middleware de errores
 
