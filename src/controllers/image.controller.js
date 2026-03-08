374a412e1300ad6fea527acd5eb74dd15a34d481
sabuthess <dilan.developer22@gmail.com>
2026-03-08 15:28:27 -0500

    Refactor images controller and user controller

--- src/controllers/image.controller.js
+++ src/controllers/image.controller.js
@@ -11,127 +11,252 @@ const storage = multer.diskStorage({
 });
 const upload = multer({ storage });
 
-export const uploadImage = (req, res) => {
-	upload.single("file")(req, res, async (err) => {
-		if (err)
-			return res
-				.status(500)
-				.json({ message: "Error al subir la imagen", error: err });
-		if (!req.file)
-			return res
-				.status(400)
-				.json({ message: "No se ha subido ninguna imagen" });
+export const ImageController = {
+	uploadImage: async (req, res) => {
+		upload.single("file")(req, res, async (err) => {
+			if (err)
+				return res
+					.status(500)
+					.json({ message: "Error al subir la imagen", error: err });
+			if (!req.file)
+				return res
+					.status(400)
+					.json({ message: "No se ha subido ninguna imagen" });
 
-		const { user_id, title, location, tags } = req.body;
-		const file_path = `uploads/${req.file.filename}`;
-		const file_name = req.file.filename;
+			const { user_id, title, location, tags } = req.body;
+			const file_path = `uploads/${req.file.filename}`;
+			const file_name = req.file.filename;
+
+			try {
+				const result = await imageModel.create({
+					user_id,
+					title,
+					location,
+					file_path,
+					file_name,
+					tags,
+				});
+				res
+					.status(200)
+					.json({ message: "Imagen subida correctamente", image: result });
+			} catch (error) {
+				res.status(500).json({ error: "Error al guardar la imagen" });
+			}
+		});
+	},
+
+	getAllImages: async (req, res) => {
+		try {
+			const results = await imageModel.getAll();
+			res.status(200).json(results);
+		} catch (error) {
+			res.status(500).json({ error: "Error al obtener las imágenes" });
+		}
+	},
+
+	getASingleImage: async (req, res) => {
+		const { id } = req.params;
+		try {
+			const result = await imageModel.getASingleImage(id);
+			if (!result)
+				return res.status(404).json({ message: "Imagen no encontrada" });
+			res.status(200).json(result);
+		} catch (error) {
+			console.error("Error en getASingleImage:", error);
+			res.status(500).json({ error: "Error al obtener la imagen" });
+		}
+	},
+
+	getSearchedImages: async (req, res) => {
+		const page = parseInt(req.query.page) || 1;
+		const limit = parseInt(req.query.limit) || 10;
+		const offset = (page - 1) * limit;
+		const search = req.query.search;
 
 		try {
-			const result = await imageModel.create({
-				user_id,
-				title,
-				location,
-				file_path,
-				file_name,
-				tags,
+			const rows = await imageModel.getSearchedImages({ search, limit, offset });
+			const total = await imageModel.countImages(search);
+			res.json({
+				data: rows,
+				total,
+				currentPage: page,
+				totalPages: Math.ceil(total / limit),
 			});
-			res
-				.status(200)
-				.json({ message: "Imagen subida correctamente", image: result });
 		} catch (error) {
-			res.status(500).json({ error: "Error al guardar la imagen" });
+			res.status(500).json({ error: "Error al obtener imágenes" });
 		}
-	});
-};
+	},
 
-export const getAllImages = async (req, res) => {
-	try {
-		const results = await imageModel.getAll();
-		res.status(200).json(results);
-	} catch (error) {
-		res.status(500).json({ error: "Error al obtener las imágenes" });
-	}
-};
+	getImagesByUser: async (req, res) => {
+		const { user_id } = req.params;
+		try {
+			const results = await imageModel.getImagesByUser(user_id);
+			res.status(200).json(results);
+		} catch (error) {
+			res.status(500).json({ error: "Error al obtener imágenes del usuario" });
+		}
+	},
 
-export const getASingleImage = async (req, res) => {
-	const { id } = req.params;
-	try {
-		const result = await imageModel.getASingleImage(id);
-		if (!result)
-			return res.status(404).json({ message: "Imagen no encontrada" });
-		res.status(200).json(result);
-	} catch (error) {
-		console.error("Error en getASingleImage:", error);
-		res.status(500).json({ error: "Error al obtener la imagen" });
-	}
-};
+	deleteImage: async (req, res) => {
+		const { user_id } = req.body;
+		const { id: image_id } = req.params;
+		console.log(req.body);
+		try {
+			const deleted = await imageModel.deleteImage(image_id, user_id);
+			if (!deleted)
+				return res.status(404).json({ message: "No se pudo eliminar la imagen" });
+			res.json({ message: "Imagen eliminada correctamente" });
+		} catch (error) {
+			res.status(500).json({ error: "Error del servidor" });
+		}
+	},
 
-export const getSearchedImages = async (req, res) => {
-	const page = parseInt(req.query.page) || 1;
-	const limit = parseInt(req.query.limit) || 10;
-	const offset = (page - 1) * limit;
-	const search = req.query.search;
+	getImageById: async (req, res) => {
+		const imageId = req.params.id;
+		const userId = req.user?.id;
 
-	try {
-		const rows = await imageModel.getSearchedImages({ search, limit, offset });
-		const total = await imageModel.countImages(search);
-		res.json({
-			data: rows,
-			total,
-			currentPage: page,
-			totalPages: Math.ceil(total / limit),
-		});
-	} catch (error) {
-		res.status(500).json({ error: "Error al obtener imágenes" });
-	}
-};
+		try {
+			const [rows] = await db.query("SELECT * FROM images WHERE id = ?", [
+				imageId,
+			]);
 
-export const getImagesByUser = async (req, res) => {
-	const { user_id } = req.params;
-	try {
-		const results = await imageModel.getImagesByUser(user_id);
-		res.status(200).json(results);
-	} catch (error) {
-		res.status(500).json({ error: "Error al obtener imágenes del usuario" });
-	}
-};
+			if (rows.length === 0) {
+				return res.status(404).json({ message: "Imagen no encontrada" });
+			}
 
-export const deleteImage = async (req, res) => {
-	const { user_id } = req.body;
-	const { id: image_id } = req.params;
-	console.log(req.body);
-	try {
-		const deleted = await imageModel.deleteImage(image_id, user_id);
-		if (!deleted)
-			return res.status(404).json({ message: "No se pudo eliminar la imagen" });
-		res.json({ message: "Imagen eliminada correctamente" });
-	} catch (error) {
-		res.status(500).json({ error: "Error del servidor" });
-	}
-};
+			const image = rows[0];
 
-export const getImageById = async (req, res) => {
-	const imageId = req.params.id;
-	const userId = req.user?.id;
+			const isFavorited = userId
+				? await imageFavoritesModel.hasUserFavorite(imageId, userId)
+				: false;
 
-	try {
-		const [rows] = await db.query("SELECT * FROM images WHERE id = ?", [
-			imageId,
-		]);
+			res.status(200).json({ ...image, isFavorited });
+		} catch (error) {
+			console.error("Error al obtener la imagen:", error);
+			res.status(500).json({ message: "Error interno del servidor" });
+		}
+	},
 
-		if (rows.length === 0) {
-			return res.status(404).json({ message: "Imagen no encontrada" });
+	addToFavorites: async (req, res) => {
+		const user_id = req.user?.id || req.body.user_id;
+		const image_id = req.params.id;
+
+		if (!user_id || !image_id) {
+			return res.status(400).json({ error: "user_id e image_id son requeridos" });
 		}
 
-		const image = rows[0];
+		try {
+			await imageFavoritesModel.addFavorite(image_id, user_id);
+			res.status(201).json({ message: "Añadido a favoritos" });
+		} catch (error) {
+			if (error.code === "ER_DUP_ENTRY") {
+				return res.status(409).json({ message: "Ya diste a favoritos" });
+			}
+			console.error("Error en addFavorite:", error);
+			res.status(500).json({ error: "Error al añadir a favoritos" });
+		}
+	},
 
-		const isFavorited = userId
-			? await imageFavoritesModel.hasUserFavorite(imageId, userId)
-			: false;
+	removeFromFavorites: async (req, res) => {
+		const image_id = req.params.id;
+		const user_id = req.user?.id || req.body.user_id;
 
-		res.status(200).json({ ...image, isFavorited });
-	} catch (error) {
-		console.error("Error al obtener la imagen:", error);
-		res.status(500).json({ message: "Error interno del servidor" });
+		if (!user_id || !image_id) {
+			return res.status(400).json({ error: "user_id e image_id son requeridos" });
+		}
+		try {
+			await imageFavoritesModel.removeFavorite(image_id, user_id);
+			res.status(200).json({ message: "Quitado de favoritos" });
+		} catch (error) {
+			console.error("Error en removeFromFavorites:", error);
+			res.status(500).json({ error: "Error al quitar de favoritos" });
+		}
+
+
+
+
+	},
+
+	checkIfUserFavorite: async (req, res) => {
+		const image_id = req.params.id;
+		const user_id = req.user?.id || req.query.user_id;
+
+		if (!user_id || !image_id) {
+			return res.status(400).json({ error: "user_id e image_id son requeridos" });
+		}
+
+		try {
+			const favorited = await imageFavoritesModel.hasUserFavorite(
+				image_id,
+				user_id
+			);
+			res.json({ hasFavorite: favorited });
+		} catch (error) {
+			console.error("Error en checkIfUserFavorite:", error);
+			res.status(500).json({ error: "Error al verificar favorito del usuario" });
+		}
+	},
+
+	addLike: async (req, res) => {
+		const user_id = req.user?.id || req.body.user_id;
+		const image_id = req.params.id;
+
+		if (!user_id || !image_id) {
+			return res.status(400).json({ error: "user_id e image_id son requeridos" });
+		}
+
+		try {
+			await imageLikeModel.addLike(image_id, user_id);
+			res.status(201).json({ message: "Like registrado" });
+		} catch (error) {
+			if (error.code === "ER_DUP_ENTRY") {
+				return res.status(409).json({ message: "Ya diste like" });
+			}
+			console.error("Error en addLike:", error);
+			res.status(500).json({ error: "Error al registrar el like" });
+		}
+	},
+
+	removeLike: async (req, res) => {
+		const user_id = req.user?.id || req.body.user_id;
+		const image_id = req.params.id;
+
+		if (!user_id || !image_id) {
+			return res.status(400).json({ error: "user_id e image_id son requeridos" });
+		}
+
+		try {
+			await imageLikeModel.removeLike(image_id, user_id);
+			res.status(200).json({ message: "Like eliminado" });
+		} catch (error) {
+			console.error("Error en removeLike:", error);
+			res.status(500).json({ error: "Error al eliminar el like" });
+		}
+	},
+	
+	checkIfUserLiked: async (req, res) => {
+		const image_id = req.params.id;
+		const user_id = req.user?.id || req.query.user_id;
+
+		if (!user_id || !image_id) {
+			return res.status(400).json({ error: "user_id e image_id son requeridos" });
+		}
+
+		try {
+			const liked = await imageLikeModel.hasUserLiked(image_id, user_id);
+			res.json({ hasLiked: liked });
+		} catch (error) {
+			console.error("Error en checkIfUserLiked:", error);
+			res.status(500).json({ error: "Error al verificar like del usuario" });
+		}
 	}
-};
+	,
+}
+
+
+
+
+
+
+
+
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,127 +2,159 @@ import bcrypt from "bcrypt";
 import { User } from "../models/user.model.js";
 import jwt from "jsonwebtoken";
 
-// ✅ Registro de usuario
-export const registerUser = async (req, res) => {
-	const { first_name, last_name, username, email, password } = req.body;
 
-	if (!first_name || !last_name || !username || !email || !password) {
-		return res
-			.status(400)
-			.json({ message: "Todos los campos son obligatorios" });
-	}
+export const UserController = {
+	registerUser: async (req, res) => {
+		const { first_name, last_name, username, email, password } = req.body;
 
-	try {
-		const [emailResults, usernameResults] = await Promise.all([
-			User.findByEmail(email),
-			User.findByUsername(username),
-		]);
-
-		if (emailResults.length > 0) {
-			return res.status(400).json({ message: "El correo ya está registrado" });
-		}
-
-		if (usernameResults.length > 0) {
+		if (!first_name || !last_name || !username || !email || !password) {
 			return res
 				.status(400)
-				.json({ message: "El nombre de usuario ya está en uso" });
+				.json({ message: "Todos los campos son obligatorios" });
 		}
 
-		const hashedPassword = await bcrypt.hash(password, 10);
+		try {
+			const [emailResults, usernameResults] = await Promise.all([
+				User.findByEmail(email),
+				User.findByUsername(username),
+			]);
 
-		await User.create(first_name, last_name, username, email, hashedPassword);
+			if (emailResults.length > 0) {
+				return res.status(400).json({ message: "El correo ya está registrado" });
+			}
 
-		res.status(201).json({ message: "Usuario registrado correctamente" });
-	} catch (err) {
-		console.error("Error en el registro:", err);
-		res.status(500).json({ error: "Error del servidor" });
-	}
-};
+			if (usernameResults.length > 0) {
+				return res
+					.status(400)
+					.json({ message: "El nombre de usuario ya está en uso" });
+			}
 
-// ✅ Login de usuario
-export const loginUser = async (req, res) => {
-	const { email, password } = req.body;
+			const hashedPassword = await bcrypt.hash(password, 10);
 
-	try {
-		const results = await User.findByEmail(email);
+			await User.create(first_name, last_name, username, email, hashedPassword);
 
-		if (results.length === 0) {
-			return res.status(401).json({ message: "Usuario no encontrado" });
+			res.status(201).json({ message: "Usuario registrado correctamente" });
+		} catch (err) {
+			console.error("Error en el registro:", err);
+			res.status(500).json({ error: "Error del servidor" });
+		}
+	},
+
+	loginUser: async (req, res) => {
+		const { email, password } = req.body;
+
+		try {
+			const results = await User.findByEmail(email);
+
+			if (results.length === 0) {
+				return res.status(401).json({ message: "Usuario no encontrado" });
+			}
+
+			const user = results[0];
+			const isMatch = await bcrypt.compare(password, user.password);
+
+			if (!isMatch) {
+				return res.status(401).json({ message: "Contraseña incorrecta" });
+			}
+
+			const token = jwt.sign(
+				{ id: user.id, email: user.email },
+				process.env.JWT_SECRET,
+				{ expiresIn: "1h" }
+			);
+
+			res.json({ message: "¡Login exitoso!", token, user });
+		} catch (err) {
+			console.error("Error al iniciar sesión:", err);
+			res.status(500).json({ message: "Error del servidor" });
+		}
+	},
+
+	FindUserbyId: async (req, res) => {
+		const { id } = req.params;
+
+		try {
+			const results = await User.findById(id);
+
+			if (results.length === 0) {
+				return res.status(404).json({ message: "Usuario no encontrado" });
+			}
+
+			const user = results[0];
+			res.json({ message: "¡Usuario encontrado!", user });
+		} catch (err) {
+			console.error("Error al buscar usuario por ID:", err);
+			res.status(500).json({ message: "Error del servidor" });
+		}
+	},
+
+	FindByUsername: async (req, res) => {
+		const { username } = req.params;
+
+		try {
+			const results = await User.findByUsername(username);
+
+			if (results.length === 0) {
+				return res
+					.status(404)
+					.json({ message: "Nombre de usuario no encontrado" });
+			}
+
+			const user = results[0];
+			res.json({ message: "¡Nombre de usuario en uso!", user });
+		} catch (err) {
+			console.error("Error al buscar por username:", err);
+			res.status(500).json({ message: "Error del servidor" });
+		}
+	},
+
+	userProfile: (req, res) => {
+		const token = req.headers["authorization"];
+
+		if (!token) {
+			return res.status(403).json({ message: "Token requerido" });
 		}
 
-		const user = results[0];
-		const isMatch = await bcrypt.compare(password, user.password);
+		jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+			if (err) {
+				return res.status(403).json({ message: "Token inválido" });
+			}
 
-		if (!isMatch) {
-			return res.status(401).json({ message: "Contraseña incorrecta" });
+			res.json({ message: "Acceso permitido", user: decoded });
+		});
+	},
+
+	getUserFavorites: async (req, res) => {
+		const user_id = req.user?.id;
+
+		if (!user_id) {
+			return res.status(400).json({ error: "user_id es requerido" });
 		}
 
-		const token = jwt.sign(
-			{ id: user.id, email: user.email },
-			process.env.JWT_SECRET,
-			{ expiresIn: "1h" }
-		);
+		try {
+			const results = await imageFavoritesModel.getFavoritesByUser(user_id);
+			res.status(200).json(results);
+		} catch (err) {
+			console.error("Error al obtener imágenes favoritas: ", err);
+			res.status(500).json({ error: "Error al obtener las imágenes favoritas" });
+		}
+	},
+	
+	getUserLikes: async (req, res) => {
+		const user_id = req.user?.id;
 
-		res.json({ message: "¡Login exitoso!", token, user });
-	} catch (err) {
-		console.error("Error al iniciar sesión:", err);
-		res.status(500).json({ message: "Error del servidor" });
-	}
-};
-
-// ✅ Buscar usuario por ID
-export const FindUserbyId = async (req, res) => {
-	const { id } = req.params;
-
-	try {
-		const results = await User.findById(id);
-
-		if (results.length === 0) {
-			return res.status(404).json({ message: "Usuario no encontrado" });
+		if (!user_id) {
+			return res.status(400).json({ error: "user_id es requerido" });
 		}
 
-		const user = results[0];
-		res.json({ message: "¡Usuario encontrado!", user });
-	} catch (err) {
-		console.error("Error al buscar usuario por ID:", err);
-		res.status(500).json({ message: "Error del servidor" });
-	}
-};
-
-// ✅ Buscar por username
-export const FindByUsername = async (req, res) => {
-	const { username } = req.params;
-
-	try {
-		const results = await User.findByUsername(username);
-
-		if (results.length === 0) {
-			return res
-				.status(404)
-				.json({ message: "Nombre de usuario no encontrado" });
+		try {
+			const results = await imageLikeModel.getLikesByUser(user_id);
+			res.status(200).json(results);
+		} catch (err) {
+			console.error("Error las imagenes que me gustaron: ", err);
+			res
+				.status(500)
+				.json({ error: "Error al obtener las imágenes que me gustaron" });
 		}
-
-		const user = results[0];
-		res.json({ message: "¡Nombre de usuario en uso!", user });
-	} catch (err) {
-		console.error("Error al buscar por username:", err);
-		res.status(500).json({ message: "Error del servidor" });
-	}
-};
-
-// ✅ Perfil del usuario usando JWT
-export const userProfile = (req, res) => {
-	const token = req.headers["authorization"];
-
-	if (!token) {
-		return res.status(403).json({ message: "Token requerido" });
-	}
-
-	jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-		if (err) {
-			return res.status(403).json({ message: "Token inválido" });
-		}
-
-		res.json({ message: "Acceso permitido", user: decoded });
-	});
-};
+	},
+}
